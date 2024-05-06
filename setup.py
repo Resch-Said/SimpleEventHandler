1395b4fffeeafb150eda276085d1820e65b2968f
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-05-06 05:12:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SimpleEventHandler",
+    version="0.1",
+    packages=find_packages(),
+    url="https://github.com/Resch-Said/SimpleEventHandler",
+    license="MIT",
+    author="Resch Said",
+    description="Just a very simple event handler.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
