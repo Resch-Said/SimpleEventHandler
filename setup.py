6af6a8a5581f9bcf25ded539e52f3664e1ba3aa8
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-05-06 05:21:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
 
 setup(
-    name="SimpleEventHandler",
-    version="0.1",
-    packages=find_packages(),
-    url="https://github.com/Resch-Said/SimpleEventHandler",
-    license="MIT",
-    author="Resch Said",
-    description="Just a very simple event handler.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    name="SimpleEventHandler",  # Required
+    version="0.1",  # Required
+    packages=find_packages(where="src"),  # Required
+    python_requires=">=3.9, <4",
 )
